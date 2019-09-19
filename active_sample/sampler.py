24def8594557863e6cbec567681382d9811233b0
Billy Su <g4691821@gmail.com>
2019-09-19 16:04:44 +0800

    Add init_all mode
    Select all minority class samples at first time, then add majority
    samples in each rounds.

--- active_sample/sampler.py
+++ active_sample/sampler.py
@@ -3,9 +3,10 @@ import pandas as pd
 from math import ceil
 
 class ActiveSampler(object):
-    def __init__(self, num_sample_round=20, majority_ratio=5):
+    def __init__(self, num_sample_round=20, majority_ratio=5, init_all=True):
         self.num_sample_round = num_sample_round
         self.majority_ratio = majority_ratio
+        self.init_all = init_all
         
     def select(self, x, y, minority_label=1):
         self.minority_label = minority_label
@@ -19,23 +20,43 @@ class ActiveSampler(object):
         self.sampled_x = pd.DataFrame()
         self.sampled_y = pd.Series()
         self.false_prob_dist = None
+
+        if self.init_all:
+            self.sampled_x = self.sampled_x.append(self.minority)
+            self.sampled_y = self.sampled_y.append(y.loc[self.minority.index.tolist()])
+
+            maj_bth_idxes = np.random.choice(self.majority.index.tolist(), size=self.minority.shape[0])
+            self.sampled_x = self.sampled_x.append(self.majority.loc[maj_bth_idxes])
+            self.sampled_y = self.sampled_y.append(y.loc[maj_bth_idxes])
+            yield self.sampled_x, self.sampled_y
+
+            # Resample majority set
+            self.sampled_x = pd.DataFrame()
+            self.sampled_y = pd.Series()
+            self.false_prob_dist = None
+
+            self.sampled_x = self.sampled_x.append(self.minority)
+            self.sampled_y = self.sampled_y.append(y.loc[self.minority.index.tolist()])
         
         for i in range(self.num_sample_round):
-            min_bth_idxes = np.random.choice(self.minority.index.tolist(),
-                                                size=min(self.round_batch, self.minority.shape[0]),
-                                                replace=False)
             maj_bth_idxes = np.random.choice(self.majority.index.tolist(),
                                                  size=min(self.round_batch*self.majority_ratio, self.majority.shape[0]),
                                                  replace=False,
                                                  p=self.false_prob_dist)
 
-            self.sampled_x = self.sampled_x.append(self.minority.loc[min_bth_idxes])
             self.sampled_x = self.sampled_x.append(self.majority.loc[maj_bth_idxes])
-            self.sampled_y = self.sampled_y.append(y.loc[min_bth_idxes])
             self.sampled_y = self.sampled_y.append(y.loc[maj_bth_idxes])
-
-            self.minority = self.minority.drop(index=min_bth_idxes)
             self.majority = self.majority.drop(index=maj_bth_idxes)
+
+            if not self.init_all:
+                min_bth_idxes = np.random.choice(self.minority.index.tolist(),
+                                                    size=min(self.round_batch, self.minority.shape[0]),
+                                                    replace=False)
+                self.sampled_x = self.sampled_x.append(self.minority.loc[min_bth_idxes])
+                self.sampled_y = self.sampled_y.append(y.loc[min_bth_idxes])
+
+                self.minority = self.minority.drop(index=min_bth_idxes)
+
             yield self.sampled_x, self.sampled_y
     
     def update_dist(self, scores):
